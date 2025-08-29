61a0f2206062c5651f5160435dd292375e9bb968
Sangeeta Bhatnagar <bhatnagars@esuhsd.org>
2025-08-28 19:31:45 -0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-//  base: '/PrimitiveTypesTracingGame/',   // 👈 must match your repo name exactly
+  base: '/PrimitiveTypesTracingGame/',   // 👈 must match your repo name exactly
 })
